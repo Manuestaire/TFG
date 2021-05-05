3a0c240d12a39896adfad8ccad4a02c79cd2783e
Manu <manuelalberto.ruedas@simloc.es>
2021-05-05 12:11:24 +0200

    Track offline analysis

---
+++ aux_functions.py
@@ -0,0 +1,60 @@
+import numpy
+import time
+import pandas as pd
+
+from scipy import stats
+
+def getTimeNow():
+    return time.strftime("%Y%m%d_%H%M%S")
+
+def lognorm_estimate(data):
+    data = data.replace(0,0.5)
+    lnx = data.apply(numpy.log)
+    mean=lnx.sum()/lnx.size
+    aux=((lnx-mean)**2)
+    variance= aux.sum()/aux.size
+    return mean,variance
+
+def getUniformParams(sample : pd.Series):
+    a=0
+    b=0
+    if(sample.size>2):
+        a=sample.dropna().min()
+        b=sample.dropna().max()
+    else:
+        print("ERROR: Sample size is too small")
+    return a,b
+
+def TTest(a : pd.Series,b : pd.Series,alpha=0.05): #95% confidence (two tailed)
+    num_t= a.mean()-b.mean()
+    s_num=(a.size-1)*a.var()+(b.size-1)*b.var()
+    df= a.size+b.size-2
+    sp=numpy.sqrt((s_num/df))
+    t=num_t/(sp*numpy.sqrt((1/a.size)+(1/b.size)))
+
+    cv = stats.t.ppf(1.0 - alpha, df)
+    p = (1 - stats.t.cdf(abs(t),df=df))*2
+    print("cv = " + str(cv))
+    print("t = " + str(t))
+    print("p = " + str(p))
+    # interpret via critical value
+    if abs(t) <= cv:
+        print('Accept null hypothesis. (critical value)')
+    else:
+        print('Reject the null hypothesis. (critical value)')
+    # interpret via p-value
+    if p > alpha:
+        print('Accept null hypothesis. (p-value)')
+    else:
+        print('Reject the null hypothesis. (p-value)')
+
+    return t,cv,p
+
+def cvmTest(sample : pd.Series, dist: stats.rv_discrete.cdf):
+    n = sample.size
+    aux = (2*numpy.arange(1,n+1,1)-1)/(2*n)
+
+    values=numpy.sort(sample.dropna())
+    dist_values= dist(values)
+    t = (1/(12*n)) + numpy.sum((aux-dist_values)**2)
+    return t
---
+++ offline_analysis.py
@@ -0,0 +1,222 @@
+import numpy
+import pandas as pd
+import matplotlib.pyplot as plt
+import os
+import math
+
+from scipy import stats
+
+from aux_functions import *
+
+class paramData:
+    """"Class wrapper for analysis data returned by the analysis"""
+    passive_tech: tuple
+    bid_tech: tuple
+
+    mining_payoff_mean: float = None
+    mining_payoff_variance: float = None
+    t_stat: float = None
+    crit_value: float = None
+    p_value: float = None
+
+    def __init__(self,passive:tuple,bid:tuple,mining_mean,mining_var,ttest:tuple):
+        self.passive_tech = passive
+        self.bid_tech = bid
+        self.mining_payoff_mean = mining_mean
+        self.mining_payoff_variance = mining_var
+        self.t_stat = ttest[0]
+        self.crit_value = ttest[1]
+        self.p_value = ttest[2]
+
+    def to_series(self):
+        data=[getTimeNow(),self.passive_tech,self.bid_tech,self.mining_payoff_mean,self.mining_payoff_variance,self.t_stat,self.crit_value,self.p_value]
+        df=pd.Series(data,index=['date','passive_tech','bid_tech','mining_payoff_mean','mining_payoff_variance','t_stat','critical_value','p_value'])
+        return df
+
+    def to_file(self):
+        timestr = time.strftime("%Y%m%d_%H%M%S")
+        outdir = "./analysis"
+        if not os.path.exists(outdir):
+            os.mkdir(outdir)
+        filepath=outdir+'/'+'results.csv'
+        if os.path.exists(filepath):
+            print('Saving to existing file')
+            df = pd.read_csv(filepath,sep=';',index_col=0)
+            df = df.append(self.to_series().to_frame().T,ignore_index=True)
+        else:
+            print('doesn\'t exists')
+            df = self.to_series().to_frame().T
+        df.to_csv(filepath,sep=';')
+
+
+class offlineAnalysis:
+    old_sample = pd.DataFrame()
+    new_sample = pd.DataFrame()
+    complete_sample = pd.DataFrame()
+
+    def __init__(self):
+        frames=[]
+        for root, dirs, filenames in os.walk('./data'):
+            for name in filenames:
+                if(name.endswith('.csv')):
+                    path = root+'/'+name
+                    print(path)
+                    file_df = pd.read_csv(path,sep=';',index_col=0)                    
+                    file_df.columns = file_df.columns.str.replace(" ", "")
+                    frames.append(file_df) #array de dataframes
+                else:
+                    print("UNKNOWN FILE EXTENSION:"+root+'/'+name)
+        if (len(frames)>0):
+            self.complete_sample = pd.concat(frames)
+            self.old_sample= pd.concat(frames[:2])
+            self.new_sample= pd.concat(frames[-2:])
+
+        self.complete_sample.to_csv('result_db.csv',sep=';')
+        print("NO MORE FILES TO PARSE")
+
+
+def analyze(sample_df : pd.DataFrame):
+        print("OFFLINE ANALYSYS RUNNING!!")
+        
+        if sample_df.size > 0 :
+            # we need to drop the values where the round does not advance
+            #(self.sample_df.loc[self.sample_df['auction_round']==1])['last_mining_payoff'].mean()
+            
+            ###### BEGIN base_reward ######
+            base_reward=(sample_df.loc[sample_df['auction_round']==1])['base_reward'].dropna()
+            br_bins=numpy.arange(-1,base_reward.max())+0.5
+            base_reward.hist(bins=br_bins, density=True) 
+            
+            #parametric estimation
+            r_mean,r_var=lognorm_estimate(base_reward)
+            print('base_reward mean:'+str(r_mean)+' var:'+str(r_var))
+            
+            #plot
+            #p_range=numpy.linspace(-1,base_reward.max(),base_reward.max()*2)
+            r_range=numpy.concatenate((numpy.linspace(0,math.floor(base_reward.max()/2),int(base_reward.max()*2)),numpy.linspace(math.ceil(base_reward.max()/2),base_reward.max())))
+            r_dist=stats.lognorm(r_var,scale=numpy.exp(r_mean))
+            rshape,rloc,rscale = stats.lognorm.fit(base_reward)
+            r_mean2=numpy.log(rscale)
+            r_dist2=stats.lognorm.pdf(r_range,rshape,loc=rloc,scale=rscale)
+            plt.plot(r_range,r_dist.pdf(r_range))
+            plt.plot(r_range,r_dist2)
+            plt.title('base_reward') 
+            plt.legend(['basic parametric estimation','scipy-fit (with location)'])
+            plt.show()
+            baser_mean=r_mean2
+            baser_var=rshape
+            br_expect=r_dist.expect()
+
+
+            ttest_result=TTest(analysis.old_sample['tech_gain_passive'].dropna(),analysis.new_sample['tech_gain_passive'].dropna())
+            ###### END base_reward ######
+
+            ###### BEGIN mining_payoff ######
+            # PLOT histograma:
+            mining_payoff=(sample_df.loc[sample_df['auction_round']==1])['last_mining_payoff'].dropna()
+            payoff_bins=numpy.arange(-1,mining_payoff.max())+0.5
+            mining_payoff.hist(bins=payoff_bins,density=True) 
+            payoff_hist = numpy.histogram(mining_payoff, bins=payoff_bins, density=True)
+            payoff_cdf=numpy.cumsum(payoff_hist[0])
+
+            
+            # PLOT pdf options
+            p_mean,p_var=lognorm_estimate(mining_payoff)
+            print('mining_payoff mean:'+str(p_mean)+' var:'+str(p_var))
+            p_range=numpy.linspace(-1,int(mining_payoff.max()),int(mining_payoff.max()*2))
+            #numpy.concatenate((numpy.linspace(0,math.floor(mining_payoff.max()/2),int(mining_payoff.max()*2)),numpy.linspace(math.ceil(mining_payoff.max()/2),mining_payoff.max())))
+            p_dist=stats.lognorm.pdf(p_range,p_var,scale=numpy.exp(p_mean))
+            plt.plot(p_range,p_dist)
+            
+            shape,floc,fscale = stats.lognorm.fit(mining_payoff)
+            mean=numpy.log(fscale)
+
+            p_mean2,p_var2=lognorm_estimate(mining_payoff-2)
+            p_dist2=stats.lognorm.pdf(p_range,p_var2,scale=numpy.exp(p_mean2))
+            plt.plot(p_range-2,p_dist2)
+            p_dist3=stats.lognorm.pdf(p_range,shape,loc=floc,scale=fscale)
+            plt.plot(p_range,p_dist3)
+
+            payoff_distribution=stats.lognorm(shape,loc=floc,scale=fscale)
+            x_axis=range(payoff_cdf.size)
+            payoff_theor_cdf=payoff_distribution.cdf(x_axis) #!!mover!
+
+            plt.title('mining payoff') 
+            plt.legend(['basic parametric estimation','parametric with displacement','scipy-fit (with location parameter)'])    
+            plt.show()
+            payoff_mean=mean
+            payoff_var=shape
+
+            # Test de Cramer von Mises
+            t_ret = stats.cramervonmises(mining_payoff,payoff_distribution.cdf)
+            t_val= cvmTest(mining_payoff,payoff_distribution.cdf)
+
+            # PLOT cdf real y estimado
+            base_reward_test=(sample_df.loc[sample_df['auction_round']==1])['base_reward'].dropna()
+            t_ret2 = stats.cramervonmises(base_reward_test,payoff_distribution.cdf)
+            plt.plot(x_axis,payoff_cdf)
+            plt.plot(x_axis,payoff_theor_cdf)
+            plt.legend(['payoff cdf','parametric-estimated payoff cdf'])
+            plt.show()
+            
+
+            # PLOT base_reward<->payoff correlation
+            a = (sample_df.loc[sample_df['auction_round']==1])
+            correlation = (a['base_reward'].shift(1)).corr(a['last_mining_payoff'])
+            print("base_reward to mining_payoff correlation:"+str(correlation))
+            b = a[['base_reward','last_mining_payoff']]
+            plt.scatter(b['base_reward'].shift(1),b['last_mining_payoff'],s=1)
+            plt.xlabel('base_reward')
+            plt.ylabel('last_mining_payoff')
+            plt.show()
+            ###### END mining_payoff ######
+
+
+            ###### BEGIN calculate expected values ######
+            #covariance=correlation*payoff_var*baser_var
+            techreward_mean=payoff_mean-2*baser_mean
+            techreward_var =payoff_var-4*baser_var
+
+            ###### END calculate expected values ######
+            
+
+            ###### BEGIN won_tech ######
+            # won_tech= self.sample_df['tech'] - self.sample_df['tech'].shift(1)  
+            # corrected_won_tech= copy.deepcopy(won_tech)
+            # corrected_won_tech[corrected_won_tech<0] = self.sample_df['tech'] #<-aquí tengo la tecnología ganada en mantenimiento cada ronda
+            fig, ax = plt.subplots(1, 2,figsize=(12,5.4))
+            won_tech_passive=sample_df['tech_gain_passive']
+            won_tech_passive.dropna().hist(bins=numpy.arange(-1,11)+0.5,density=True,ax=ax[0])
+            ax[0].set_title('tech_gain_passive')     
+            
+            won_tech_bid=sample_df['tech_gain_bid']
+            won_tech_bid.dropna().hist(bins=numpy.arange(-1,11)+0.5,density=True,ax=ax[1])
+            ax[1].set_title('tech_gain_bid') 
+            plt.show()
+            ###### END won_tech ######
+
+
+
+            return_data = paramData(getUniformParams(won_tech_passive),getUniformParams(won_tech_bid),p_mean,p_var,ttest_result)
+            return_data.to_file()
+
+            #new_df = pd.concat([won_tech,corrected_won_tech],axis=1) #debugging
+            #corrected_won_tech.plot.density(ind=range(0,11))
+
+            # shifted_df = self.sample_df[['last_winning_bid','last_winning_bidders']].shift(-1)
+            #bid_won_tech= self.sample_df.loc['Manu' in shifted_df['last_winning_bidders']]
+            #aux=(shifted_df['last_winning_bidders'].dropna().str.contains('Manu')).fillna(value=False)
+
+
+        
+            # (self.sample_df.loc[self.sample_df['tech']!=self.sample_df['tech_at_join']!=])['tech_at_join'].dropna().hist(bins='auto')
+            # plt.figure(1)
+        else:
+            print('Could not generate BD_dataframe')
+
+
+
+
+analysis = offlineAnalysis()
+analyze(analysis.complete_sample)
+TTest(analysis.old_sample['tech_gain_passive'].dropna(),analysis.new_sample['tech_gain_passive'].dropna())
