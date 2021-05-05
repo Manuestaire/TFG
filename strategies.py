8f72a30fd4f7770986c956c9504d90d2756e65a1
Manu <manuelalberto.ruedas@simloc.es>
2021-05-05 12:17:11 +0200

    Update info processing in strategy

--- strategies.py
+++ strategies.py
@@ -18,6 +18,8 @@ import os
 import time
 import copy
 
+from aux_functions import *
+
 
 class Strategy(object):
     """
@@ -45,31 +47,84 @@ class Strategy(object):
 class Manu(Strategy):
     game_df = pd.DataFrame()
     bd_df = pd.DataFrame()
-    def bid(self, private_information, public_information):
+    players_tech_count = {}
 
-        df_players = pd.DataFrame(public_information['players']).squeeze()
+    prev_tech = 0
+    launched = False
+    def addRow(self, private_information, public_information):
+        tech_gain = private_information['tech']-self.prev_tech if not self.launched else private_information['tech']
+        df_players = pd.DataFrame(public_information['players']).reset_index(drop=True)
         public_info = copy.deepcopy(public_information)
         del public_info['players']
-        public_info = pd.Series(public_info)
 
-        row_df= pd.concat([public_info,df_players]).to_frame().T
-#hacer dos series y: https://stackoverflow.com/questions/38109102/combining-two-series-into-a-dataframe-row-wise
+        row=pd.DataFrame.from_dict(public_info,orient='index').T.infer_objects()
+        row_df= pd.concat([row,df_players],axis=1)
 
-        self.game_df = self.game_df.append(row_df,ignore_index=True)
+
+        row_df.at[0,'tech' if public_info['auction_round']<=1 else 'tech_at_join']=private_information['tech']
+        #hacer dos series y: https://stackoverflow.com/questions/38109102/combining-two-series-into-a-dataframe-row-wise
+
+        row_df.at[0,'tech_gain_passive' if public_info['auction_round']<=1 else 'tech_gain_bid']=tech_gain
+        self.game_df = self.game_df.append(row_df,ignore_index=True) #add row to game_df
+        return row_df
+
+    def bid(self, private_information, public_information):
+
+        self.addRow(private_information, public_information)
+        
+        if(public_information['auction_round']==1): #Hubo lanzamiento en la ronda anterior
+            if(public_information['last_launchers'] != None):
+                # print('hola')
+                for launcher in public_information['last_launchers']:
+                     self.players_tech_count[launcher]=0
+            for player, data in public_information['players'].items():
+                if(data['bankroll']<0):
+                    self.players_tech_count[player]=0 #lo fuerzo a cero para evitar que influya en la toma de decision
+                else:
+                    self.players_tech_count[player]+=1
+
+
+
+        #self.bd_df.loc[self.bd_df.index[-2],'last_winning_bid']=5
+
         ##self.df = self.df.insert(-1,value=df_players)
         #self.df = self.df.append(df_players,ignore_index=True)
         #self.df = pd.concat([self.df,df_players],axis=1)
 
         launch=False
 
+        #Memory vars:
+        self.launched=launch
+        self.prev_tech=private_information['tech']
+
         return 4, launch
 
     def join_launch(self, private_information, public_information):
+        
+        if(public_information['last_winning_bidders']!=None):
+            for player in public_information['last_winning_bidders']:
+                self.players_tech_count[player]+=1
+        
+        self.game_df.loc[self.game_df.index[-1],'tech_at_join']=private_information['tech']
+
+        if 'Manu' in public_information['last_winning_bidders']:
+            tech_gain_bid = private_information['tech'] - self.prev_tech
+            self.game_df.loc[self.game_df.index[-1],'tech_gain_bid']=tech_gain_bid
+
+
+        self.prev_tech=private_information['tech']
         print("JOIN LAUNCH?")
-        return True
+
+        joining_launch=True
+        self.launched|=joining_launch
+
+        return joining_launch
 
     def begin(self, private_information, public_information):
         print("GAME ABOUT TO BEGIN!!")
+        self.players_tech_count = {p:0 for p in public_information['players'].keys()}
+        # for name in public_information['players'].keys():
+        #     self.players_tech_count[name]=0
         frames=[]
         for root, dirs, filenames in os.walk('./data'):
             for name in filenames:
@@ -82,17 +137,49 @@ class Manu(Strategy):
                     frames.append(file_df)
                 else:
                     print("UNKNOWN FILE EXT:"+root+'/'+name)
-        self.bd_df = pd.concat(frames)
+        if (len(frames)>0):
+            self.bd_df = pd.concat(frames)
         self.bd_df.to_csv('result_db.csv',sep=';')
         print("NO MORE FILES TO PARSE")
 
-        # we need to drop the values where the round does not advance
-        #(self.bd_df.loc[self.bd_df['auction_round']==1])['last_mining_payoff'].mean()
-        #histograma:
-        #(self.bd_df.loc[self.bd_df['auction_round']==1])['last_mining_payoff'].hist(bins='auto')
-        #plt.show()
+        if self.bd_df.size > 0 :
+            # we need to drop the values where the round does not advance
+            #(self.bd_df.loc[self.bd_df['auction_round']==1])['last_mining_payoff'].mean()
+            #histograma:
+            m_mean,m_var=lognorm_estimate((self.bd_df.loc[self.bd_df['auction_round']==1])['last_mining_payoff'].dropna())
+            print('mining_payoff mean:'+str(m_mean)+' var:'+str(m_var))
+            
+            # won_tech= self.bd_df['tech'] - self.bd_df['tech'].shift(1)  
+            # corrected_won_tech= copy.deepcopy(won_tech)
+            # corrected_won_tech[corrected_won_tech<0] = self.bd_df['tech'] #<-aquí tengo la tecnología ganada en mantenimiento cada ronda
+            won_tech_passive=self.bd_df['tech_gain_passive']
+            won_tech_passive.dropna().hist(bins=numpy.arange(-1,11)+0.5,density=True)
+            plt.title('tech_gain_passive')     
+            # plt.show()
+            won_tech_bid=self.bd_df['tech_gain_bid']
+            won_tech_bid.dropna().hist(bins=numpy.arange(-1,11)+0.5,density=True)
+            plt.title('tech_gain_bid') 
+            # plt.show()
+
+            #new_df = pd.concat([won_tech,corrected_won_tech],axis=1) #debugging
+            #corrected_won_tech.plot.density(ind=range(0,11))
+
+            # shifted_df = self.bd_df[['last_winning_bid','last_winning_bidders']].shift(-1)
+            #bid_won_tech= self.bd_df.loc['Manu' in shifted_df['last_winning_bidders']]
+            #aux=(shifted_df['last_winning_bidders'].dropna().str.contains('Manu')).fillna(value=False)
+
+
+            (self.bd_df.loc[self.bd_df['auction_round']==1])['last_mining_payoff'].dropna().hist(bins='auto') 
+            plt.title('mining payoff')     
+            # plt.show()
+            # (self.bd_df.loc[self.bd_df['tech']!=self.bd_df['tech_at_join']!=])['tech_at_join'].dropna().hist(bins='auto')
+            # plt.figure(1)
+            print('BD_dataframe generated successfully')
+        else:
+            print('Could not generate BD_dataframe')
 
     def end(self, private_information, public_information):
+        self.addRow(private_information, public_information)
         timestr = time.strftime("%Y%m%d_%H%M%S")
         outdir = "./data"
         if not os.path.exists(outdir):
@@ -173,7 +260,7 @@ class PassiveLauncher(Strategy):
     """
 
     def bid(self, private_information, public_information):
-        amount = min(private_information['bankroll'], public_information['last_winning_bid'] - 1)
+        amount = min(private_information['bankroll'], round(public_information['last_winning_bid'] *0.9))
         launching = False
         return int(amount), launching
 
