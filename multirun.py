f1d3dff4e314fb25d6f6ba755d39fcaa59ed8a36
Manu <manuelalberto.ruedas@simloc.es>
2021-05-25 01:08:41 +0200

    Modify "multirun.py" to run the optimization problem

--- multirun.py
+++ multirun.py
@@ -1,13 +1,121 @@
 import subprocess
+import time
+import os
+import pandas as pd
+from scipy.optimize import minimize
+
+def multiprocess():
+    rango=15
+    p = [None]*rango
+    files = [None]*rango
+    for i in range(rango):
+        # try:
+        #     out = subprocess.run("python smp.py", check=True, shell=True)
+        #     print(out)
+        # except Exception as e:
+        #     print("EXCEPTION: "+e)
+        files[i]=open('log'+str(i),'w')
+        try:
+            p[i]=subprocess.Popen(["python","smp.py"],stdout=files[i],stderr=files[i])
+            time.sleep(1)
+        except Exception as e:
+            print("EXCEPTION: "+e)
+    # else:
+    #     print("Finished RUN")
+
+    status = [None]*rango
+    while None in status:
+        for i in range(rango):
+            print('process '+str(i)+': '+str(status[i]))
+            status[i]=p[i].poll()
+        time.sleep(5)
+    for process in p:
+        process.wait()
+
+def singleprocess():
+    for i in range(5):
+        try:
+            out = subprocess.run("python smp.py", check=True, shell=True)
+            print(out)
+        except Exception as e:
+            print("EXCEPTION: "+e)
+    else:
+        print("Finished RUN")
+
+def runcampaign(params,multiprocess_run=True):
+    #primero escribo parámetros a archivo
+    timestr = time.strftime("%Y%m%d_%H%M%S")
+    outdir = "./data/campaign_"+timestr
+    if not os.path.exists(outdir):
+        os.mkdir(outdir)
+    filepath='campaigndata.csv'
+
+    data=[timestr, params[0], params[1], params[2], params[3],0]
+    row=pd.Series(data,index=['date','joindecision_threshold','launchdecision_threshold','raisebid_factor','risk_factor','objective'])
+    if os.path.exists(filepath):
+        print('Saving to existing file')
+        df = pd.read_csv(filepath,sep=';',index_col=0)
+        df = df.append(row.to_frame().T,ignore_index=True) 
+    else:
+        df = row.to_frame().T
+    df.to_csv(filepath,sep=';')
+
+    #luego corro la campaña
+    if multiprocess_run:
+        multiprocess()
+    else:
+        singleprocess()
+
+    #luego analizo la función objetivo:
+    frames=[]
+    for root, dirs, filenames in os.walk(outdir):
+        for name in filenames:
+            if(name.endswith('.csv')):
+                try:
+                    path = root+'/'+name
+                    print(path)
+                    file_df = pd.read_csv(path,sep=';',index_col=0)                    
+                    file_df.columns = file_df.columns.str.replace(" ", "")
+                    frames.append(file_df) #array de dataframes
+                except Exception:
+                    print('Error al leer archivo ',path)
+            else:
+                print("UNKNOWN FILE EXTENSION:"+root+'/'+name)
+    if (len(frames)>0):
+        #aux[aux['AggressiveLauncher']<0]['round'].iloc[0] #pilla la ronda en la que entra en bancarrota
+        obj = 0
+        games_in_campaign=0
+        for frame in frames:
+            aux = frame.loc[frame['Manu']<0,'round']
+            z = 0
+            if aux.size!=0:
+                z=201-aux.iloc[0] # positivo, mayor cuanto antes nos hayan eliminado
+            else:
+                z=(frame.tail(1).loc[:,"SpongeBob":"Evie"].values.max()-frame.tail(1)['Manu'].item())/200 #0 si soy yo // positivo estoy por detrás (maximizo la distancia con el segundo)
+            obj+=z
+            games_in_campaign+=1
+
+        df.iloc[-1,-1]=obj/games_in_campaign
+        df.to_csv(filepath,sep=';')
+
+    return obj/games_in_campaign
 
 
-for x in range(3):
-    try:
-        out = subprocess.run("python smp.py", check=True)
-        print(out)
-    except Exception as e:
-        print("EXCEPTION: "+e)
-else:
-    print("Finished RUN")
+def main():
+    print("Hello World!")
+    initial_guess=[0.5,0.5,0.5,0.5]
+    boundaries=((0,1),(0,1),(0,1),(0,1))
+    res=minimize(runcampaign,x0=[initial_guess],bounds=boundaries,options={"maxiter":4})
+    print(res)
 
+if __name__ == "__main__":
+    main()
+
+
+
+        
+
+
+                
+            
 
--- strategies.py
+++ strategies.py
@@ -11,6 +11,7 @@ well as .broadcast(), which receives human readable messages
 about the game's progress.
 """
 
+# from ntpath import join
 import numpy
 import math
 import pandas as pd
@@ -60,6 +61,17 @@ class Manu(Strategy):
     prev_bid = 2
     eliminated_player_count=0
     launched = False
+
+    UPKEEP_COST= 5
+    LAST_ROUND = 201
+    TECH_EXPECTATION = 5
+
+    campaign_folder = ''
+    joindecision_threshold=0.3
+    launchdecision_threshold=0.6
+    raisebid_factor = 0.6
+    risk_factor=0.5
+
     def addRow(self, private_information, public_information):
         try:
             tech_gain = private_information['tech']-self.prev_tech if not self.launched else private_information['tech']
@@ -80,8 +92,97 @@ class Manu(Strategy):
         self.game_df = self.game_df.append(row_df,ignore_index=True) #add row to game_df
         return row_df
 
+    def bidStrategy(self,public_information,private_information,riskfactor,optimismfactor):
+
+
+        actual_expected_reward = public_information['base_reward']+self.unkn_expect #cuánto espero ganar del próximo meteorito
+        total_spent=0 #TODO
+        total_spent/public_information['round'] #compara con cuánto están gastando los demás (si les va bien les copio)
+        public_information['last_mining_payoff']
+        self.tech_cost
+        private_information['tech'] #total tecnología actual
+
+        #factores psicológicos:
+        if( (actual_expected_reward) * optimismfactor > 2*self.payoff_expect):
+            cs_er = max(actual_expected_reward, 2*self.payoff_expect * (1+optimismfactor))
+        else:
+            cs_er = actual_expected_reward * (0.5+optimismfactor)
+        cr_es = max(self.br_expect,cs_er)
+
+        #estimo cuánta tech tienen los demás jugadores:
+        player_tech_expected=copy.deepcopy(self.players_tech_count)
+        player_tech_expected.update((k,self.TECH_EXPECTATION*5) for k,v in player_tech_expected.items()) 
+        for k,v in player_tech_expected.items():
+            if(self.players_tech_count['Manu']>=2 and \
+            v>2*private_information['tech']):
+                player_tech_expected[k]=None #descarto a este jugador porque es demasiado conservador (puede ser un observador)
+        player_tech_expected['Manu']=None # a mí también me descarto
+
+
+        remaining_rounds= (self.LAST_ROUND-public_information['round'])  
+        ps=remaining_rounds*(self.UPKEEP_COST) #coste de no lanzar hasta el final de partida
+        pr=remaining_rounds*self.payoff_expect #total estimated price pool remaining
+        total_spent = 0 #TODO_ total gastado a lo largo de la partida
+        pr+private_information['bankroll']-(ps+total_spent)>0 #esto me dice si voy bien o mal
+
+
+
+        if private_information['tech'] > max(player_tech_expected.values()): #quiero mantener la ventaja
+            bid_amount = 5 
+        #si gano esta puja debería lanzar o unirme
+        
+        launch=False
+        if private_information['tech'] >= max(player_tech_expected.values())*(1-riskfactor): #TODO: modificar para que entre sobretodo al principio (me sale un 10 en la primera ronda) si la recompensa es baja no me interesa
+            launch=True
+
+        potential_profit=cr_es-(self.UPKEEP_COST+bid_amount)
+        if potential_profit > 0:  # me sale rentable lanzar
+            launch=True
+
+        if public_information['round']==self.LAST_ROUND:
+            launch=True
+
+        if launch: #si me interesa lanzar seguramente quiera apostar
+            if private_information['tech']+self.TECH_EXPECTATION > max(player_tech_expected.values()) and private_information['tech'] < max(player_tech_expected.values()): 
+                bid_amount = min(cr_es,actual_expected_reward)-self.UPKEEP_COST #si con una puja espero superar la tech de los demás estoy dispuesto a pagar
+            elif private_information['tech']  > max(player_tech_expected.values()) or  private_information['tech']<=2 :
+                bid_amount = self.UPKEEP_COST #si ya estamos por delante pujamos el coste de mantenimiento
+            else:
+                bid_amount = min(self.game_df['last_winning_bid'].tail(-1).min(),self.UPKEEP_COST)
+        else: #no lanzamos este turno
+            if private_information['tech']+self.TECH_EXPECTATION > max(player_tech_expected.values()) and private_information['tech'] < max(player_tech_expected.values()):
+                bid_amount = min(self.game_df['last_winning_bid'].tail(-1).min(),self.UPKEEP_COST)
+            else:
+                bid_amount = 1
+            
+
+        ## JOIN STRATEGY:
+        join_launch=False
+        if private_information['tech'] < player_tech_expected.values().min()*(1+riskfactor) or launch:
+            join_launch = True
+        elif actual_expected_reward < self.payoff_expect:
+            join_launch = False
+        else: 
+            join_launch = private_information['tech'] < self.payoff_expect
+        
+            
+
+
+        #a = min(max(self.br_expect,numpy.log(public_information['base_reward'])), self.payoff_expect) * confidencefactor # qué peso le damos a este factor (log -> quiero dar más peso si hay más dispersión)
+        #b = max(0,(cr_es-self.tech_cost)) * riskfactor        # cuánto estamos dispuestos a perder
+        #c = self.tech_cost * memoryfactor      #
+
+
+
+        return
+
+
+
+    
+
     def bid(self, private_information, public_information):
 
+        #### BEGIN CRONS ####
         self.addRow(private_information, public_information)
         self.tech_cost+=5
         
@@ -97,6 +198,7 @@ class Manu(Strategy):
                     self.players_tech_count[player]=0 #lo fuerzo a cero para evitar que influya en la toma de decision
                 else:
                     self.players_tech_count[player]+=1
+        #### END CRONS ####
 
 
 
@@ -111,7 +213,7 @@ class Manu(Strategy):
         launch=False
         
         player_tech_expected=copy.deepcopy(self.players_tech_count)
-        player_tech_expected.update((k,v*5) for k,v in player_tech_expected.items()) 
+        player_tech_expected.update((k,v*self.TECH_EXPECTATION) for k,v in player_tech_expected.items()) 
 
         launchdecision_threshold=0.6
         basereward_mod=numpy.sqrt((public_information['base_reward']/self.br_expect)+0.125)
@@ -146,8 +248,8 @@ class Manu(Strategy):
 
         bid_amount=max(0,bid_amount)
 
-        risk_factor=1 #should be close to 1
-        if(self.tech_cost>(public_information['base_reward']+self.unkn_expect)*risk_factor):
+        risk_factor=0.5 #should be close to 1
+        if(self.tech_cost>(public_information['base_reward']+self.unkn_expect)*(risk_factor+0.5)):
             bid_amount=1
 
         # for sublist in counts.index.values:
@@ -190,7 +292,7 @@ class Manu(Strategy):
         player_tech_expected.update((k,v*5) for k,v in player_tech_expected.items()) 
         
         #joining_launch = (private_information['tech']/sum(player_tech_expected.values()))*(public_information['base_reward']/br_expect) > 0.4 #+failure_chance #and expected reward is greater than tech invested?
-        joindecision_threshold=0.4
+        joindecision_threshold=0.3
         basereward_mod=numpy.sqrt((public_information['base_reward']/self.br_expect)+0.125)
         joining_launch = (private_information['tech']/sum(player_tech_expected.values()))*basereward_mod > joindecision_threshold
         if joining_launch:
@@ -209,6 +311,16 @@ class Manu(Strategy):
         # for name in public_information['players'].keys():
         #     self.players_tech_count[name]=0
 
+        #### carga campaña ####
+        filepath="campaigndata.csv"
+        if os.path.exists(filepath):
+            campaign_data = pd.read_csv(filepath,sep=';',index_col=0).iloc[-1]
+            self.campaign_folder='/campaign_'+campaign_data['date']
+        self.joindecision_threshold=campaign_data['joindecision_threshold']
+        self.launchdecision_threshold=campaign_data['launchdecision_threshold']
+        self.raisebid_factor = campaign_data['raisebid_factor']
+        self.risk_factor=campaign_data['risk_factor']
+
         #### BEGIN Read persistent data from offline analysis ####
         filepath="./analysis/_results.csv"
         if os.path.exists(filepath):
@@ -216,6 +328,10 @@ class Manu(Strategy):
             distrib_data=persistent_data.tail(1)
             base_reward_dist=lognormData(distrib_data['base_reward_mean'],distrib_data['base_reward_variance'], distrib_data['base_reward_location'])
             payoff_dist = lognormData(distrib_data['mining_payoff_mean'],distrib_data['mining_payoff_variance'], distrib_data['mining_payoff_location'])
+
+            # self.br_expect=base_reward_dist.distribution().expect()
+            # self.payoff_expect= payoff_dist.distribution().expect()
+            # self.unkn_expect=  self.payoff_expect -  self.br_expect
         else:
             print("ERROR: couldn't find persistance data from offline analysis")
         #### END Read persistent data ####
@@ -226,10 +342,14 @@ class Manu(Strategy):
                 if(name.endswith('.csv')):
                     path = root+'/'+name
                     print(path)
-                    file_df = pd.read_csv(path,sep=';',index_col=0)                    
-                    file_df.columns = file_df.columns.str.replace(" ", "")
-                    #self.bd_df = self.bd_df.append(file_df,ignore_index=True,sort=False)[file_df.columns.tolist()]
-                    frames.append(file_df)
+                    try:
+                        file_df = pd.read_csv(path,sep=';',index_col=0)                    
+                        file_df.columns = file_df.columns.str.replace(" ", "")
+                        #self.bd_df = self.bd_df.append(file_df,ignore_index=True,sort=False)[file_df.columns.tolist()]
+                        frames.append(file_df)
+                    except Exception as e:
+                        print('Error al leer archivo ',path)
+                        print(e)
                 else:
                     print("UNKNOWN FILE EXT:"+root+'/'+name)
         if (len(frames)>0):
@@ -238,17 +358,29 @@ class Manu(Strategy):
         print("NO MORE FILES TO PARSE")
 
         #### BEGIN fitness tests ####
-        sample_df=pd.concat(frames[-2:])
+        sample_df=pd.concat(frames[-5:])
         mining_payoff=(sample_df.loc[sample_df['auction_round']==1])['last_mining_payoff'].dropna()
         t_stat = cvmTest(mining_payoff,payoff_dist.distribution().cdf)
         if(t_stat>0.46136):
             print("Null hypothesis rejected, PAYOFF sample does not belong to given distribution")
-            input("Press enter to continue")
+            payoff_bins=numpy.arange(-1,mining_payoff.max())+0.5
+            mining_payoff.hist(bins=payoff_bins,density=True) 
+            p_range=numpy.linspace(-1,int(mining_payoff.max()),int(mining_payoff.max()*2))
+            p_dist=payoff_dist.distribution().pdf(p_range)
+            # plt.plot(p_range,p_dist)
+            # plt.show()
+            # input("Press enter to continue")
         base_reward=(sample_df.loc[sample_df['auction_round']==1])['base_reward'].dropna()
-        t_stat = cvmTest(mining_payoff,payoff_dist.distribution().cdf)
+        t_stat = cvmTest(base_reward,base_reward_dist.distribution().cdf)
         if(t_stat>0.46136):
             print("Null hypothesis rejected, BASE REWARD sample does not belong to given distribution")
-            input("Press enter to continue")
+            br_bins=numpy.arange(-1,base_reward.max())+0.5
+            base_reward.hist(bins=br_bins,density=True) 
+            p_range=numpy.linspace(-1,int(base_reward.max()),int(base_reward.max()*2))
+            p_dist=base_reward_dist.distribution().pdf(p_range)
+            # plt.plot(p_range,p_dist)
+            # plt.show()
+            # input("Press enter to continue")
         #### END fitness tests ####
 
         if self.bd_df.size > 0 :
@@ -292,9 +424,9 @@ class Manu(Strategy):
         ax=None
         self.game_df.loc[:,"SpongeBob":"Evie"].plot(ax=ax)
         self.game_df[["round","auction_round"]].plot(ax=ax)
-        plt.show()
+        # plt.show()
         timestr = time.strftime("%Y%m%d_%H%M%S")
-        outdir = "./data"
+        outdir = "./data"+self.campaign_folder
         if not os.path.exists(outdir):
             os.mkdir(outdir)
         path = outdir+'/'+timestr+'_result.csv'
@@ -380,6 +512,7 @@ class PassiveLauncher(Strategy):
     def bid(self, private_information, public_information):
         amount = min(private_information['bankroll'], round(public_information['last_winning_bid'] *0.9))
         launching = False
+        #launching = numpy.random.uniform(0,1)>0.5
         return int(amount), launching
 
     def join_launch(self, private_information, public_information):
