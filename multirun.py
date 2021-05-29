5bef909fa17519ddd7cf35b354a0128441b298df
Manu <manuelalberto.ruedas@simloc.es>
2021-05-29 16:07:05 +0200

    Refactor multiprocess campaign runs & fix issues

--- multirun.py
+++ multirun.py
@@ -1,39 +1,41 @@
 import subprocess
 import time
 import os
+import math
 import pandas as pd
 from scipy.optimize import minimize
 
 def multiprocess():
-    rango=15
-    p = [None]*rango
-    files = [None]*rango
-    for i in range(rango):
-        # try:
-        #     out = subprocess.run("python smp.py", check=True, shell=True)
-        #     print(out)
-        # except Exception as e:
-        #     print("EXCEPTION: "+e)
-        files[i]=open('log'+str(i),'w')
-        try:
-            p[i]=subprocess.Popen(["python","smp.py"],stdout=files[i],stderr=files[i])
-            time.sleep(1)
-        except Exception as e:
-            print("EXCEPTION: "+e)
-    # else:
-    #     print("Finished RUN")
-
-    status = [None]*rango
-    while None in status:
-        for i in range(rango):
-            print('process '+str(i)+': '+str(status[i]))
-            status[i]=p[i].poll()
-        time.sleep(5)
-    for process in p:
-        process.wait()
+    games=100
+    concurent_process=8
+    p = [None]*concurent_process
+    files = [None]*concurent_process
+    for j in range(int(round(games/concurent_process,0))):
+        for i in range(concurent_process):
+            # try:
+            #     out = subprocess.run("python smp.py", check=True, shell=True)
+            #     print(out)
+            # except Exception as e:
+            #     print("EXCEPTION: "+e)
+            files[i]=open('log'+str(i),'w')
+            try:
+                p[i]=subprocess.Popen(["python","smp.py"],stdout=files[i],stderr=files[i])
+                time.sleep(0.05)
+            except Exception as e:
+                print("EXCEPTION: "+e)
+            # else:
+            #     print("Finished RUN")
+        status = [None]*concurent_process
+        while None in status:
+            for i in range(concurent_process):
+                # print('process '+str(i)+': '+str(status[i]))
+                status[i]=p[i].poll()
+            # time.sleep(5)
+        for process in p:
+            process.wait()
 
 def singleprocess():
-    for i in range(5):
+    for i in range(50):
         try:
             out = subprocess.run("python smp.py", check=True, shell=True)
             print(out)
@@ -43,15 +45,24 @@ def singleprocess():
         print("Finished RUN")
 
 def runcampaign(params,multiprocess_run=True):
-    #primero escribo parámetros a archivo
+    ## primero escribo parámetros a archivo
     timestr = time.strftime("%Y%m%d_%H%M%S")
-    outdir = "./data/campaign_"+timestr
-    if not os.path.exists(outdir):
-        os.mkdir(outdir)
+    basedir = "./data/campaign_"
+    outdir = basedir+timestr
+    # if not os.path.exists(outdir):
+    #     os.mkdir(outdir)
+    char = 'a'
+    while os.path.exists(outdir):
+        outdir = basedir+timestr+'_'+char
+        char = chr(ord(char) + 1)
+    os.mkdir(outdir)
+    
+    ## Vuelco parámetros a csv
     filepath='campaigndata.csv'
 
     data=[timestr, params[0], params[1], params[2], params[3],0]
     row=pd.Series(data,index=['date','joindecision_threshold','launchdecision_threshold','raisebid_factor','risk_factor','objective'])
+    df = None
     if os.path.exists(filepath):
         print('Saving to existing file')
         df = pd.read_csv(filepath,sep=';',index_col=0)
@@ -60,13 +71,13 @@ def runcampaign(params,multiprocess_run=True):
         df = row.to_frame().T
     df.to_csv(filepath,sep=';')
 
-    #luego corro la campaña
+    ## luego corro la campaña
     if multiprocess_run:
         multiprocess()
     else:
         singleprocess()
 
-    #luego analizo la función objetivo:
+    ## luego analizo la función objetivo:
     frames=[]
     for root, dirs, filenames in os.walk(outdir):
         for name in filenames:
@@ -75,7 +86,7 @@ def runcampaign(params,multiprocess_run=True):
                     path = root+'/'+name
                     print(path)
                     file_df = pd.read_csv(path,sep=';',index_col=0)                    
-                    file_df.columns = file_df.columns.str.replace(" ", "")
+                    # file_df.columns = file_df.columns.str.replace(" ", "")
                     frames.append(file_df) #array de dataframes
                 except Exception:
                     print('Error al leer archivo ',path)
@@ -91,10 +102,13 @@ def runcampaign(params,multiprocess_run=True):
             if aux.size!=0:
                 z=201-aux.iloc[0] # positivo, mayor cuanto antes nos hayan eliminado
             else:
-                z=(frame.tail(1).loc[:,"SpongeBob":"Evie"].values.max()-frame.tail(1)['Manu'].item())/200 #0 si soy yo // positivo estoy por detrás (maximizo la distancia con el segundo)
-            obj+=z
-            games_in_campaign+=1
-
+                z=(frame.loc[frame.index[-1],"SpongeBob":"Evie"].values.max()-frame['Manu'].iloc[-1].item())/200 #0 si soy yo // positivo estoy por detrás (maximizo la distancia con el segundo)
+                if math.isnan(z):
+                    z=(frame.loc[frame.index[-2],"SpongeBob":"Evie"].values.max()-frame['Manu'].iloc[-2].item())/200 #workaround para evitar NaN
+            if not math.isnan(z):
+                obj+=z
+                games_in_campaign+=1
+        print(z)
         df.iloc[-1,-1]=obj/games_in_campaign
         df.to_csv(filepath,sep=';')
 
